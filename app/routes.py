d9880b467d581cfc5327e5eb488be3ca5496d0e8
FeiHaiRui <1876019890@qq.com>
2021-07-23 16:19:55 +0800

    修改前端页面

--- app/routes.py
+++ app/routes.py
@@ -3,10 +3,23 @@
 # @Author  : 费海瑞
 # @File    : routes.py
 # @Software: PyCharm
+from flask import render_template
 
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-   return "Hello 费海瑞喜欢学习！"
+   user = {'username':'songbo'}
+   # 创建一个列表：帖子。里面元素是两个字典，每个字典里元素还是字典，分别作者、帖子内容。
+   posts = [
+      {
+         'author': {'username': 'John'},
+         'body': 'Beautiful day in Portland!'
+      },
+      {
+      'author': {'username': 'Susan'},
+      'body': 'The Avengers movie was so cool!'
+      }
+   ]
+   return render_template("index.html",title='home',user=user,posts=posts)
